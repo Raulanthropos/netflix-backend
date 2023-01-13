d889553a996566a181899e1561aee76521cde917
Raulanthropos <ipsichias@yandex.com>
2023-01-13 18:54:36 +0200

    posted_the_documentation

--- src/api/info/index.js
+++ src/api/info/index.js
@@ -6,7 +6,7 @@ const infoRouter = express.Router();
 
 infoRouter.get("/docs", (req, res, next) => {
     try {
-        res.redirect(`https://api.postman.com/collections/24358339-a6f1de49-86c9-43ec-b435-54145fc5590c?access_key=PMAT-01GPNKQP7S2QXZ32KAY3SY7VKT`)
+        res.redirect(`https://documenter.getpostman.com/view/24358339/2s8ZDSbkB1`)
     } catch (error) {
         next(error)
     }
