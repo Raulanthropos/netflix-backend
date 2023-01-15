3c0bbc36ba574cb9aca98e564abb47a3af002506
Raulanthropos <ipsichias@yandex.com>
2023-01-15 18:51:54 +0200

    fixed the documentation link

--- src/api/info/index.js
+++ src/api/info/index.js
@@ -6,7 +6,7 @@ const infoRouter = express.Router();
 
 infoRouter.get("/docs", (req, res, next) => {
     try {
-        res.redirect(`https://documenter.getpostman.com/view/24358339/2s8ZDSbkB1`)
+        res.redirect(`https://documenter.getpostman.com/view/24358339/2s8ZDSbkB1#cd6e83fa-568a-4f96-808d-17298536586f`)
     } catch (error) {
         next(error)
     }
